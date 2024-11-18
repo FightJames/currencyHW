a8df9add21c1e9c50bb32b09b7f7e6f66b7d2c91
james.lin <james.lin@jameslins-MacBook-Pro.local>
2024-11-18 21:26:46 +0800

    [refactor]: Delete redundant code.

--- app/src/main/java/com/james/crypto/ui/Navigation.kt
+++ app/src/main/java/com/james/crypto/ui/Navigation.kt
@@ -1,39 +1,6 @@
 package com.james.crypto.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Serializer
-
-
-@Composable
-fun NavigationConfig(
-    navController: NavHostController,
-    modifier: Modifier,
-    indexPage: @Composable () -> Unit,
-    currencyListPage: @Composable (CurrencyList) -> Unit,
-) {
-    NavHost(
-        navController = navController,
-        startDestination = Index,
-        modifier = modifier
-    ) {
-        composable<Index> {
-            indexPage()
-        }
-        composable<CurrencyList> { backStackEntry ->
-            currencyListPage(backStackEntry.toRoute<CurrencyList>())
-        }
-    }
-
-}
-
-@Serializable
-object Index
 
 @Serializable
 data class CurrencyList(val currencyType: CurrencyType): java.io.Serializable
